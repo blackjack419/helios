7b5a7e2c5a1085366678fd116da08518adfd8557
Angelo Piazza <ajpiazza19@hotmail.com>
2021-10-09 23:34:06 -0400

    Kernel is now working again Although, I need to do a clean rebuild to verify

---
+++ board/beaglebone/local.mk
@@ -0,0 +1,7 @@
+# Specify paths for already existing pkgs
+# These will not be downloaded by buildroot, but be used as-is
+
+#BAREBOX_OVERRIDE_SRCDIR=$(BR2_EXTERNAL_HELIOS_PATH)/package/barebox
+#BAREBOX_AUX_OVERRIDE_SRCDIR=$(BR2_EXTERNAL_HELIOS_PATH)/package/barebox
+#LINUX_OVERRIDE_SRCDIR=$(BR2_EXTERNAL_HELIOS_PATH)/package/linux-ti
+
