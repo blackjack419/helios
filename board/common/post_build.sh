b8f9ef61cb996e1f518609205cf5ab3e01a7811e
Angelo Piazza <ajpiazza19@hotmail.com>
2021-10-30 18:08:04 -0400

    Reverted to default skeleton Login prompt now properly displays at boot Added ID to busybox binary Moved patches out of board/common and into global /patches directory Changed a few openrc settings Added post_build.sh for future rootfs deletions

---
+++ board/common/post_build.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# Called after Buildroot builds all the selected software, but before
+# the rootfs images are assemebled. Runs with the main Buildroot tree
+# as the current working directory.
+#
+# The path to the target filesystem is passed as the first argument
+# to this script.
+#
+# You can remove or modify any file in your target filesystem. However,
+# you should use this feature with care. Whenever you find that a
+# certain package generates wrong or unneeded files, you should fix
+# that package rather than work around it with this post-build cleanup.
+
+# Remove undesired folders from default skeleton
+rm -rf ${TARGET_DIR}/opt
+rm -rf ${TARGET_DIR}/media
+
+# Remove unwanted openrc init files
+openrc_config=${TARGET_DIR}/etc/conf.d
+openrc_runtime=${TARGET_DIR}/etc/runlevels
+
+rm -f ${openrc_runtime}/boot/binfmt
+rm -f ${openrc_runtime}/boot/termencoding
+rm -f ${openrc_runtime}/boot/save-termencoding
+rm -f ${openrc_runtime}/default/netmount
+rm -f ${openrc_config}/consolefont
